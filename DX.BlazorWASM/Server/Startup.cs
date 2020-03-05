87155a92c50bec0ce75c0d0b22b3677fd4a624b8
Don Wibier <don@anykey.nl>
2020-03-05 14:12:33 +0100

    Put some use full code in the NavMenu.razor (WASM) file to determine if you're looking at the WASM or Server version. Small error fixed in _Host.cshtml on Blazor Server project (css links were not referenced correctly)

--- DX.BlazorWASM/Server/Startup.cs
+++ DX.BlazorWASM/Server/Startup.cs
@@ -2,6 +2,7 @@ using DX.Blazor.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.ResponseCompression;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using System.Linq;
@@ -10,10 +11,19 @@ namespace DX.BlazorWASM.Server
 {
 	public class Startup
 	{
+		public IConfiguration Configuration { get; }
+
+		public Startup(IConfiguration configuration)
+		{
+			Configuration = configuration;
+		}
 		// This method gets called by the runtime. Use this method to add services to the container.
 		// For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
 		public void ConfigureServices(IServiceCollection services)
 		{
+			string connStrName = "DefaultConnection";
+			string connStr = Configuration.GetConnectionString(connStrName);
+
 			services.AddMvc();
 			services.AddResponseCompression(opts =>
 			{
